54e3b2d3dcc5f6c56e26e37b27a6157ca4bb4dbb
dmlocal <dmlocal>
2017-06-06 14:14:07 -0700

    added createPost feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=wiziple';
 
-export function fetchPost() {
-  const url = `${ROOTURL}/posts${API_KEY}`;
+export function fetchPosts() {
+  const url = `${ROOT_URL}/posts${API_KEY}`;
   const request = axios.get(url);
 
   return {
     type:FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values, callback) {
+  const url = `${ROOT_URL}/posts${API_KEY}`;
+  const request = axios.post(url, values).then(() => callback());
+
+  return {
+    type:CREATE_POST,
     payload: request
   }
 }
--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,13 +1,55 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() { //same as ngOnInit
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Write
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return { posts: state.posts};
+}
+
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+
+/*
+import { bindActionCreators } from 'redux';
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( { fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps) (PostsIndex);
+*/
+
